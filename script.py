fbc06a13fb63807525642477bc2423db9423257a
dexxxed <dexxxed.app@gmail.com>
2019-09-26 20:08:33 +0300

    testing new features

--- script.py
+++ script.py
@@ -6,16 +6,16 @@ def get_training_and_testing_lists(list):
     return jumbled_list[:int(0.7*len(list))], jumbled_list[int(0.7*len(list)):]
 
 
-def changer(list):
-    for element in list:
-        if element.find('setosa') != -1:
-            element = element.replace('setosa', '1')
-        elif element.find('versicolor') != -1:
-            element = element.replace('versicolor', '2')
-        elif element.find('virginica') != -1:
-            element = element.replace('virginica', '3')
-
-    return list
+# def changer(list):
+#     for element in list:
+#         if element.find('setosa') != -1:
+#             element = element.replace('setosa', '1')
+#         elif element.find('versicolor') != -1:
+#             element = element.replace('versicolor', '2')
+#         elif element.find('virginica') != -1:
+#             element = element.replace('virginica', '3')
+#
+#     return list
 
 
 if __name__ == '__main__':
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     print(origin_list)  # Debug
     training, testing = get_training_and_testing_lists(origin_list)
     # print(len(origin_list), len(training), len(testing),  len(training) + len(testing))
-    changed_list = changer(origin_list)
-    print(changed_list)
+
+    # changed_list = changer(origin_list)
+    # print(changed_list)
 
