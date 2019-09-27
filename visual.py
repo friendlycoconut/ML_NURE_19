63a7a88d1727f5fe3c05af48674488a9ad2bcfe7
dexxxed <dexxxed.app@gmail.com>
2019-09-27 20:31:59 +0300

    visual ready

--- visual.py
+++ visual.py
@@ -25,19 +25,18 @@ plt.ylim(y_min, y_max)
 plt.xticks(())
 plt.yticks(())
 
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
+# Чтобы лучше понять взаимодействие измерений, постройте первые три измерения PCA
 fig = plt.figure(1, figsize=(8, 6))
 ax = Axes3D(fig, elev=-150, azim=110)
 X_reduced = PCA(n_components=3).fit_transform(iris.data)
 ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
            cmap=plt.cm.Set1, edgecolor='k', s=40)
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
+ax.set_title("Первые три измерения PCA")
+ax.set_xlabel("1-ое измерение")
 ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
+ax.set_ylabel("2-ое измерение")
 ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
+ax.set_zlabel("3-е измерение")
 ax.w_zaxis.set_ticklabels([])
 
 plt.show()
