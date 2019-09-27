23837924f768e627bd8d3a141b46eaa47f127852
dexxxed <dexxxed.app@gmail.com>
2019-09-27 20:28:08 +0300

    some changes

--- visual.py
+++ visual.py
@@ -1,12 +1,11 @@
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn.decomposition import PCA
 
-# import some data to play with
+# загружаем данные для обработки (они хранятся в датасетах scikit-learn)
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
+X = iris.data[:, :2]  # нам нужны только 2 первых колонки
 y = iris.target
 
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
@@ -15,11 +14,11 @@ y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 plt.figure(2, figsize=(8, 6))
 plt.clf()
 
-# Plot the training points
+
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
             edgecolor='k')
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
+plt.xlabel('Длина чашелистника')
+plt.ylabel('Ширина чашелистника')
 
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
